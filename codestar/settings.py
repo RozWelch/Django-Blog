93ae2a0c926950420b348e1546d666098361ba28
RozWelch <rozwelch@gmail.com>
2022-12-06 20:15:36 +0000

    Add models part 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
